cb37d263ab0040395059b18ef16ca78d5d113f9a
Isuru Madusanka <isurum@swivelgroup.com.au>
2023-03-01 11:14:20 +0530

    updated headers

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,4 +4,15 @@ const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
 });
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers["Access-Control-Allow-Origin"] = "*";
+    config.headers["Content-Type"] = "application/json";
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
