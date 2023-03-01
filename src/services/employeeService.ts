06b1e2758264793844b9d0687e424a6c27327e00
Isuru Madusanka <isurum@swivelgroup.com.au>
2023-03-01 11:21:34 +0530

    updated headers

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -1,18 +1,25 @@
 import axiosInstance from "@/services";
 import { IGetEmployees } from "@/types";
 
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+};
+
 export const getAllEmployees = async ({ field, sort }: IGetEmployees) => {
   return await axiosInstance.get(`/employee?field=${field}&&sort=${sort}`);
 };
 
 export const addEmployee = async (payload: any) => {
-  return await axiosInstance.post("/employee", payload);
+  return await axiosInstance.post("/employee", payload, config);
 };
 
 export const editEmployee = async (empId: string, payload: any) => {
-  return await axiosInstance.put(`/employee/${empId}`, payload);
+  return await axiosInstance.put(`/employee/${empId}`, payload, config);
 };
 
 export const deleteEmployee = async (empId: string) => {
-  return await axiosInstance.delete(`/employee/${empId}`);
+  return await axiosInstance.delete(`/employee/${empId}`, config);
 };
